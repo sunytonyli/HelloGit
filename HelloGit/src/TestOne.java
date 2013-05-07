89f2aff73cc5bae2a082121250ac442741378fd6
tony <sunytonyli@gmail.com>
2013-05-08 00:03:32 +0800

    本地检出测试

--- HelloGit/src/TestOne.java
+++ HelloGit/src/TestOne.java
@@ -5,7 +5,8 @@
 public class TestOne {
 
 	public static void main(String[] args){
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git version 1!");
+		System.out.println("Hello Git version 2!");
 	}
 	
 }
