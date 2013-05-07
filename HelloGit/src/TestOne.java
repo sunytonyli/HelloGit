ef179cdaa836e8478ecab3405dcc71ff31cd0d31
tony <sunytonyli@gmail.com>
2013-05-08 00:32:01 +0800

    test

--- HelloGit/src/TestOne.java
+++ HelloGit/src/TestOne.java
@@ -7,6 +7,7 @@ public class TestOne {
 	public static void main(String[] args){
 		System.out.println("Hello Git version 1!");
 		System.out.println("Hello Git version 2!");
+		System.out.println("Hello Git version 3!");
 	}
 	
 }
